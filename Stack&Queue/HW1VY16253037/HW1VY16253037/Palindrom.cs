9834d7d19a91e48610d9536894302684ebca9803
kadikoyluseren <kadikoyluseren@gmail.com>
2018-05-05 01:59:59 +0300

    Veri Yapıları 1.Ödev

---
+++ Stack&Queue/HW1VY16253037/HW1VY16253037/Palindrom.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace HW1VY16253037
+{
+    class Palindrom
+    {
+        public string boslukNoktalamaSil(string cumle)
+        {
+            cumle = cumle.ToLower();
+            cumle = Regex.Replace(cumle, @"(\p{P})", "");
+            cumle = cumle.Replace(" ", "");
+            return cumle;
+        }
+
+        public Stack<char> cumleToStack(string cumle)
+        {
+            Stack<char> stk = new Stack<char>();
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                stk.Push(cumle[i]);
+            }
+            return stk;
+        }
+
+        public Queue<char> cumleToQueue(string cumle)
+        {
+            Queue<char> que = new Queue<char>();
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                que.Enqueue(cumle[i]);
+            }
+            return que;
+        }
+
+        public void Karsilastir(Stack<char> stk, Queue<char> que)
+        {
+            int i = 0;
+            int j=stk.Count;
+            while (stk.Count!=0)
+            {
+                if (stk.Pop() == que.Dequeue())
+                    ++i;
+                else
+                {
+                    Console.WriteLine("Ifade palindrom degildir!");
+                    break;
+                }
+            }
+            if(i==j)
+                Console.WriteLine("Ifade palindromdur!");
+        }
+    }
+}
---
+++ Stack&Queue/HW1VY16253037/HW1VY16253037/Program.cs
@@ -0,0 +1,34 @@
+﻿
+// ÖĞRENCİ NUMARASI : 16253037
+// AD SOYAD : SEREN KADIKÖYLÜ
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1VY16253037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle;
+            Console.Write("Bir cumle girin: ");
+            cumle = Console.ReadLine();
+
+            Palindrom pal = new Palindrom();
+            cumle = pal.boslukNoktalamaSil(cumle);
+
+            Stack<char> stk = new Stack<char>();
+            stk = pal.cumleToStack(cumle);
+
+            Queue<char> que = new Queue<char>();
+            que = pal.cumleToQueue(cumle);
+
+            pal.Karsilastir(stk, que);
+
+        }
+    }
+}
